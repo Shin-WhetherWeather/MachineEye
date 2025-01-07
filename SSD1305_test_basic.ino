1e5eaf55d0a7ca6099f5134a661f1a2df593b74d
Shin-WhetherWeather <108211650+Shin-WhetherWeather@users.noreply.github.com>
2025-01-08 01:11:11 +1100

    Update SSD1305_test_basic.ino

--- SSD1305_test_basic.ino
+++ SSD1305_test_basic.ino
@@ -3,21 +3,12 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1305.h>
 
-// Used for software or hardware SPI
 #define OLED_CS 10
 #define OLED_DC 8
-
-// Used for I2C or SPI
 #define OLED_RESET 9
 
-// software SPI
-
 Adafruit_SSD1305 display(128, 32, &SPI, OLED_DC, OLED_RESET, OLED_CS);
-// hardware SPI - use 7Mhz (7000000UL) or lower because the screen is rated for 4MHz, or it will remain blank!
-// Adafruit_SSD1305 display(128, 64, &SPI, OLED_DC, OLED_RESET, OLED_CS, 7000000UL);
 
-
-//#define TEXT "    Hello, world! I am hungry and tired and upside down!    "
 
 String displayText = "";
 
@@ -57,12 +48,47 @@ void printText(uint16_t scrollInterval){
 }
 
 
+
+
+
+
+
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(115200, SERIAL_8N1, 0, 1); 
-  
-  display.begin(0x3C);
+  Serial1.begin(115200, SERIAL_8N1, 1, 0); 
+  Serial1.setTimeout(20);
 
+  //Gyroscope setup
+  //------------------------------------------------------------------
+  SPI.begin();
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
+  
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+  delay(250);
+
+  //Sets full range to 4G
+  spiWrite(0x01, 0b00000000);
+  spiWrite(0x12, 0b00000001);
+  spiWrite(0x10, 0b10011010);
+
+  digitalWrite(7, LOW);
+  delayMicroseconds(250);
+  
+  SPI.transfer(0x0F | 0b10000000);
+  int val = SPI.transfer(0x00);
+  //Serial.println(val);
+  
+  delayMicroseconds(250);
+  digitalWrite(7, HIGH);
+  SPI.endTransaction();
+  delay(25);
+  //------------------------------------------------------------------
+
+  //Display Setup
+  //------------------------------------------------------------------
+
+  display.begin(0x3C);
   display.clearDisplay();
   display.display();
   display.setTextWrap(false);
@@ -72,25 +98,84 @@ void setup() {
   x = 0;
   y = 10;
 
-  renderText("    Hello world! I am hungry and tired and upside down!!    ");
+  renderText("                   Welcome to Machine Eye! I try to make sense of the world around me.                      ");
 
   scrollTime = millis();
   updateTime = millis();
 
+  //------------------------------------------------------------------
 }
 
+void spiWrite(byte addr, byte dat){
+  //Hard coded SPI transfer function for the gyroscope
+  digitalWrite(7, LOW);
+  delayMicroseconds(1);
+  SPI.transfer(addr);
+  SPI.transfer(dat);
+  digitalWrite(7, HIGH);
+  delayMicroseconds(500); 
+}
 
-
+void read6(byte addr, int *val){
+  //Hard coded function to read all the X, Y, and Z accelerations
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
+  digitalWrite(7, LOW);
+  delayMicroseconds(1);
+  SPI.transfer(addr | 0b10000000);
+  for(byte i = 0; i < 3; i++){
+    val[i] = SPI.transfer(0xff) | (SPI.transfer(0x00) << 8);
+    if(val[i] >= 32768){
+      val[i] -= 65536;
+    }  
+  }
+  digitalWrite(7, HIGH);
+  delayMicroseconds(1);
+  SPI.endTransaction();
+}
 
 void loop() {
-  if(Serial1.available() > 0 ){
-    String incomingData;
-    incomingData = Serial1.readString();
+  String cmd = Serial1.readStringUntil('\n');
 
-    if(incomingData.length() > 0){
-      renderText(incomingData);
+  if(cmd.length()){
+    //strips the first character off the serial input
+    char code = cmd.charAt(0);
+    switch(code){
+      case 'T':{
+        //print text
+        //-------------------------------------------------------
+        cmd = cmd.substring(1);
+        renderText("                   " + cmd + "                      ");
+        Serial.println(cmd);
+        //-------------------------------------------------------
+      }break;
+      case 'G':{
+        //fetch gyro
+        //-------------------------------------------------------
+        int val[] = {0,0,0};
+        int valAvg[] = {0,0,0};
+
+        //Samples 4x and summed
+        //Averaging is done on the Raspberry Pi
+        for(int i = 0; i < 4; i++){
+          read6(0x28, val);
+          for(int i = 0; i < 3; i++){
+            valAvg[i] = valAvg[i] + val[i];
+          }
+        }
+
+        //Sends the gyroscope values over over Serial
+        Serial1.print(valAvg[0]);
+        Serial1.print(" ");
+        Serial1.print(valAvg[1]);
+        Serial1.print(" ");
+        Serial1.println(valAvg[2]);
+        Serial1.flush();
+        //-------------------------------------------------------
+      }break;
     }
   }
 
   printText(40);
+
+
 }
